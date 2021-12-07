216e359d2af254b8048f6ab3d2c8315b38c4c19f
Prajcurry091307 <77421995+Prajcurry091307@users.noreply.github.com>
2021-12-06 16:47:11 -0800

    Add files via upload

--- main,.js
+++ main,.js
@@ -1,17 +1,20 @@
 
-song="";
+beliver_song="";
+avengers_song="";
 
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
-/*scoreLeftWrist = 0;
-*/scoreRightWrist =0;
+status = "";
+scoreLeftWrist = 0;
+scoreRightWrist =0;
 
 
 function preload()
 {
-song = loadSound("music.mp3");
+ loadSound("beliver.mp3");
+ loadSound("avengers.mp3");
 }
 
 function setup()
@@ -35,9 +38,9 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        /*scoreRightWrist = results[0].pose.keypoints[10].score * 100;
+        scoreRightWrist = results[0].pose.keypoints[10].score * 100;
         scoreLeftWrist = results[0].pose.keypoints[9].score * 100;
-        */console.log("scoreRightWrist =" +scoreRightWrist +"scoreLeftWrist = "+ scoreLeftWrist);
+        console.log("scoreRightWrist =" +scoreRightWrist +"scoreLeftWrist = "+ scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = "+leftWristX +" leftWristY = "+ leftWristY);
@@ -54,16 +57,16 @@ function draw()
     fill("#ff0000");
     stroke("#ff0000");
  
-//if(scoreRightWrist>0.2)
+if(scoreRightWrist>0.2)
 
     circle(rightWristX, rightWristY,20);
 
-    if(rightWristY>0 && rightWristY>0)
+    if(rightWristX>0 && rightWristY>0)
     {
         document.getElementById("song_button").innerHTML = "Song = Believer";
         song.play('believer.mp3');
     }
-    if(leftWristY>0 && leftWristY>0)
+    if(leftWristX>0 && leftWristY>0)
     {
         document.getElementById("song_button").innerHTML = "Song = Avengers";
         song.play('avengers.mp3');
